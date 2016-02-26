8f21b8b9ed518d8aaf7bc080e25a9eefe7022649
Ciro <CiroboyBR@users.noreply.github.com>
2016-02-26 10:16:24 -0300

    Update AgendaApp.h

--- AgendaApp.h
+++ AgendaApp.h
@@ -1,11 +1,4 @@
-/***************************************************************
- * Name:      AgendaApp.h
- * Purpose:   Defines Application Class
- * Author:    Ciro Meneses (ciroccuva@hotmail.com)
- * Created:   2015-10-18
- * Copyright: Ciro Meneses (www.programacaopradiversao.blogspot.com.br)
- * License:
- **************************************************************/
+
 
 #ifndef AGENDAAPP_H
 #define AGENDAAPP_H
